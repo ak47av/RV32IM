f7654aa712c12f36494e5c74072d1d9c4e9bb178
Arun Vijay <Arun Vijay Sampath>
2025-05-21 14:31:51 +0100

    Added comments to Radix-2 SRT division

--- SRT2/SRT2.sv
+++ SRT2/SRT2.sv
@@ -1,29 +1,9 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04.04.2025 10:09:29
-// Design Name: 
-// Module Name: SRT2
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module SRT2 #(parameter N=32)(
     input logic rst, clk,
-    input logic unsigned [N-1:0] numerator, denominator,
-    output logic unsigned [N-1:0] quotient, remainder,
+    input logic signed [N-1:0] numerator, denominator,
+    output logic signed [N-1:0] quotient, remainder,
     output logic [2:0] top3bits,
     output logic [7:0] [N:0] BR,
     output logic [N:0] R,
@@ -32,24 +12,26 @@ module SRT2 #(parameter N=32)(
     output logic done
 );
     
+    // State variables
     typedef enum logic [1:0] {IDLE, RUNNING, DONE} state_t;
     state_t state;
     
-    logic [N:0] B;
-    logic [4:0] shift;
+    logic [N:0] B;      // denominator after shifting out leading zeroes
+    // Intermediate variables for calculating the leading zeroes
     logic [15:0] val16;
     logic [7:0] val8;
     logic [3:0] val4;
-    logic [4:0] result;
-    //logic [2:0] top3bits;
+    logic [4:0] result; // Holds the number of leading zeroes
     
     always_ff @(posedge clk or posedge rst) begin
+        // Return to IDLE state if rst
         if(rst) begin
             state <= IDLE;
         end else begin
+        // Begin the division process
             case(state)
                 IDLE: begin
-                    //BR <= multiplicand;
+                    // Calculate the number of leading zeroes with a combinational circuit
                     result[4] = (denominator[31:16] == 16'b0);
                     val16     = result[4] ? denominator[15:0] : denominator[31:16];
                     result[3] = (val16[15:8] == 8'b0);
@@ -59,11 +41,13 @@ module SRT2 #(parameter N=32)(
                     result[1] = (val4[3:2] == 2'b0);
                     result[0] = result[1] ? ~val4[1] : ~val4[3];
                     
-                    R = 0;
-                    Q = numerator;
-                    {R, Q} = {R,Q} << result;
+                    // Setup the Divider circuit
+                    R = 0;  // Hold the remainder
+                    Q = numerator;  // Hold the Quotient
+                    {R, Q} = {R,Q} << result;   // Shift left by the number of leading zeros
                     
                     B = denominator << result;
+                    // Table to hold the coefficients
                     BR[3'b000] <= 0;
                     BR[3'b001] <= -B;
                     BR[3'b010] <= -B;
@@ -73,6 +57,7 @@ module SRT2 #(parameter N=32)(
                     BR[3'b110] <= B;
                     BR[3'b111] <= 0;
                     
+                    // Move to the running state
                     state <= RUNNING;
                     done <= 0;
                     count <= 0;
@@ -81,7 +66,9 @@ module SRT2 #(parameter N=32)(
                 end
                 
                 RUNNING: begin
+                  // the division process goes through 32 iterations, one for every clock cycle
                     if(count < N) begin
+                    // The selection is based on the most significant 3 bits
                        top3bits = R[N:N-2];
                        {R,Q} = {R,Q} << 1;
                        if (top3bits == {3{1'b1}} || top3bits == {3{1'b0}}) begin
@@ -94,6 +81,7 @@ module SRT2 #(parameter N=32)(
                        quotient = quotient << 1;
                        R = R + BR[top3bits];
                     end else begin
+                        // If the number is negative, add B back to the remainder and -1 from the quotient
                         if (R[N] == 1) begin
                             R = R + B;
                             Q = Q - 1;
@@ -104,6 +92,7 @@ module SRT2 #(parameter N=32)(
                         remainder <= R[N-1:0];
                         quotient <= Q;
                     end
+                    // increment the count for every iteration
                     count = count + 1;
                 end
                 
