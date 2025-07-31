c61df5eedd791c69c22f12a83996507d4b98f183
Arun Vijay <arunvijay184708@gmail.com>
2025-07-31 15:22:29 +0100

    Jump instructions jal and jalr implemented and verified

--- RV32IM_Vivado/RV32IM_Vivado.srcs/sources_1/new/BranchControl.sv
+++ RV32IM_Vivado/RV32IM_Vivado.srcs/sources_1/new/BranchControl.sv
@@ -6,23 +6,38 @@ module BranchControl(
     input logic [31:0] immediate,
     input logic [31:0] PC,
     input logic [31:0] ALUoutput,
-    //input logic [31:0] rs1,
+    input logic [31:0] rs1,
     output logic [31:0] PCnext,
-    // output logic hasJumped,
-    output logic hasBranched
+    output logic hasJumped,
+    output logic hasBranched,
+    output logic [31:0] returnAddress
     );
     
-    assign PCNext = immediate + PC;
+    logic [31:0] jalrNext, jalNext, branchNext;
+    assign jalrNext = (rs1 + immediate) & ~(32'b1);
+    assign jalNext = immediate + PC;
+    assign branchNext = immediate + PC;
     
     // check if branch instruction
     logic isBranch;
     assign isBranch = brsel[2] | brsel[1] | brsel[0];
     
     assign hasBranched = ALUoutput[0] & isBranch;
+    assign hasJumped = brsel[3];
+    
+    logic isJalr;
+    assign isJalr = (brsel[3] & ~brsel[0]); 
+    assign PCnext = isJalr ? jalrNext : 
+                            (isBranch) ? branchNext : jalNext;
+    //assign PCNext = jalNext;
+    
+    //write back to rd if jmp;
+    assign returnAddress = PC + 4;
     
     always @(posedge clk) begin
         $display("[BR] immediate: %0h | PC: %0h", immediate, PC);
-        $display("[BR] PCNext: %0h | hasBranched: %0h", PCnext, hasBranched);
+        $display("[BR] JalrNext: %0h | isJalr: %0h", jalrNext, isJalr);
+        $display("[BR] PCNext: %0h | hasBranched: %0h | hasJumped: %0h", PCnext, hasBranched, hasJumped);
     end
     
 endmodule
--- RV32IM_Vivado/RV32IM_Vivado.srcs/sources_1/new/InstructionMemory.sv
+++ RV32IM_Vivado/RV32IM_Vivado.srcs/sources_1/new/InstructionMemory.sv
@@ -12,16 +12,15 @@ module InstructionMemory #(
         word_addr = addr[ADDR_WIDTH-1:2];  // divide by 4 for instruction index
 
         case (word_addr)
-            6'd0:  ins_out = 32'hEA000093;
-            6'd1:  ins_out = 32'h15C00113;
-            6'd2:  ins_out = 32'h02116233;
-            6'd3:  ins_out = 32'h02117333;
-            6'd4:  ins_out = 32'h021141B3;
-            6'd5:  ins_out = 32'h021152B3;
-            6'd6:  ins_out = 32'h021103B3;
-            6'd7:  ins_out = 32'h02111433;
-            6'd8:  ins_out = 32'h021134B3;
-            6'd9:  ins_out = 32'h02112533;
+            6'd0:  ins_out = 32'h00500193;
+            6'd1:  ins_out = 32'h00700213;
+            6'd2:  ins_out = 32'h010000EF;
+            6'd3:  ins_out = 32'h014000EF;
+            6'd4:  ins_out = 32'h00F00393;
+            6'd5:  ins_out = 32'h00E00413;
+            6'd6:  ins_out = 32'h00418333;
+            6'd7:  ins_out = 32'h00008067;
+            6'd8:  ins_out = 32'h00000013;
             default: ins_out = 32'h00000013; // NOP (addi x0,x0,0)
         endcase
     end
